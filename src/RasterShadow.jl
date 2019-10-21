af52865ef63ebd6de4fda9cfef3b834c3e1f4e69
Dag Wastberg <dwastberg@gmail.com>
2019-10-21 15:48:24 +0200

    Initial commit

--- src/RasterShadow.jl
+++ src/RasterShadow.jl
@@ -1,5 +1,75 @@
 module RasterShadow
 
-greet() = print("Hello World!")
+export shadowing
+function shadowing(dem::Array{<:AbstractFloat,2}, azimuth::Real, altitude::Real, pixel_scale::Real=1.0)
+    if azimuth == 0
+        azimuth = 0.1
+    end
+    pixel_scale = 1/pixel_scale
+    azimuth = deg2rad(azimuth)
+    altitude = deg2rad(altitude)
+
+    sizex, sizey  = size(dem)
+
+    dx::Int = 0
+    dy::Int = 0
+    dz::Float64 = 0
+    dem_max = maximum(dem)
+
+    sinazimuth = sin(azimuth)
+    cosazimuth = cos(azimuth)
+    tanazimuth = tan(azimuth)
+
+    signsinazimuth = trunc(Int, sign(sinazimuth))
+    signcosazimuth = trunc(Int, sign(cosazimuth))
+    dssin = abs((1 / sinazimuth))
+    dscos = abs((1 / cosazimuth))
+    tanaltitudebyscale = tan(altitude) / pixel_scale
+
+    demcopy = copy(dem)
+    index::Int = 1
+    while dz < dem_max && abs(dx) < sizex && abs(dy) < sizey
+        if ( (π / 4 <= azimuth && azimuth < 3 * π / 4) || (5 * π / 4 <= azimuth && azimuth < 7 * π / 4) )
+            dy = signsinazimuth * index
+            dx = -1 * signcosazimuth * abs(round(Int, index / tanazimuth))
+            ds = dssin
+        else
+            dy = signsinazimuth * abs(round(Int, index * tanazimuth))
+            dx = -1 * signcosazimuth * index
+            ds = dscos
+        end
+        dz = ds * index * tanaltitudebyscale
+
+        absdx = abs(dx)
+   	    absdy = abs(dy)
+
+        xc1 = ((dx + absdx) ÷ 2) + 1
+        yc1 = ((dy + absdy) ÷ 2) + 1
+
+        xp1 = -((dx - absdx) ÷ 2) + 1
+        xp2 = (sizex - (dx + absdx) ÷ 2)
+
+        yp1 = -((dy - absdy) ÷ 2) + 1
+        yp2 = (sizey - (dy + absdy) ÷ 2)
+
+        xc_offset = xc1 - xp1
+        yc_offset = yc1 - yp1
+        for j in yp1:yp2
+            for i in xp1:xp2
+                demcopy[i,j] = max(demcopy[i,j], dem[i + xc_offset,j + yc_offset] - dz)
+            end
+        end
+        index += 1
+
+    end
+    for i in eachindex(demcopy)
+        if demcopy[i] == dem[i]
+            demcopy[i] = 1
+        else
+            demcopy[i] = 0
+        end
+    end
+    return demcopy
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,18 @@
 using RasterShadow
 using Test
+using JLD
 
-@testset "RasterShadow.jl" begin
-    # Write your own tests here.
+@testset "basic input" begin
+    flat = ones(20,10)
+    flat_sh = shadowing(flat,45,45,1)
+    @test size(flat_sh) == size(flat)
+    @test flat_sh == ones(20,10)
+end
+
+testdata = JLD.load("testdata\\testdata.jld")
+dem = testdata["dem"]
+@testset "shadowing" begin
+    @test testdata["sh_30_30"] == shadowing(dem,30,30,0.5)
+    @test testdata["sh_150_80"] == shadowing(dem,150,80,0.5)
+    @test testdata["sh_150_80"] == shadowing(dem,150,80,0.5)
 end
